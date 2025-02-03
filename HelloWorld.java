2769f938a0a2bf06f5b89cc3239856d7f4a080ad
tassia <tassia@acaia.ca>
2025-02-03 14:08:59 -0500

    Initial sharing of project

---
+++ HelloWorld.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author Tassia Camoes Araujo
+ * @version 0.1
+ */
+public class HelloWorld
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class HelloWorld
+     */
+    public HelloWorld()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
